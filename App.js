61c502f0a5522d917003401b4269072bea644ebd
Ashir-Littra <ashir.b@littra.in>
2022-04-15 16:24:46 +0530

    finished header

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <View style={styles.body}>
       <View style={styles.header}>
-        <Text>ToDo List</Text>
+        <Text style={styles.headerText}>ToDo List</Text>
       </View>
       <View style={styles.container}>
         <View style={styles.InputContainer}>
@@ -27,7 +27,22 @@ const App = () => {
 
 const styles = StyleSheet.create({
   body: {
-    backgroundColor: 'yellow',
+    backgroundColor: 'black',
+    height: '100%',
+    width: '100%',
+  },
+  header: {
+    height: '10%',
+    width: '100%',
+    backgroundColor: 'black',
+    borderColor: 'white',
+    borderWidth: 1,
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'center',
+    marginTop: 10,
   },
 });
 export default App;
